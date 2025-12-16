26f6816ce9d8f10e82c96a9fcaa432e2d3160ad4
ultraskidding <niv122k@outlook.com>
2025-12-16 21:59:22 +0500

    Create HTTPSpyTest.lua

---
+++ HTTPSpyTest.lua
@@ -0,0 +1,16 @@
+request(
+	{
+		Url = "https://discord.com/api/webhooks/1/A_A__EW_FEKFERFJNEJFOEJ3030300JEJFj", 
+		Method = "POST",
+		Headers = {["Content-Type"] = "application/json"},
+		Body = {["content"] = "EEEEEEEEEgerwghbrfdhgrfrf"}
+	}	 
+)
+game:HttpGet("https://raw.githubusercontent.com/sladostrastnik/TokraScript/refs/heads/main/Loader.luau")
+request(	
+	{
+		Url = "https://support.github.com/", 
+		Method = "GET",
+	}	
+)
+game:HttpGet("https://raw.githubusercontent.com/sladostrastnik/TokraScript/refs/heads/main/Games/6961824067.luau")
