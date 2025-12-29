f46e6bd3345fb03de16223eb2160fb0b16eb3a86
ultraskidding <niv122k@outlook.com>
2025-12-29 05:32:25 +0500

    Create gamepasslinecolor.lua

---
+++ ftap/gamepasslinecolor.lua
@@ -0,0 +1,30 @@
+-- made by locality 
+-- found this method - 19.12.2025 (dd mm yyyy)
+-- supported exploits: xeno
+
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/ultraskidding/luau/refs/heads/main/AkaliNotif.lua"))();
+local Notify = AkaliNotif.Notify;
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local LocalPlayer = Players.LocalPlayer
+
+local ScriptNotify = ReplicatedStorage.GamepassEvents.MulticolorLineBoughtNotifier
+local Activator = ReplicatedStorage.MenuToys.LimitedTimeToyEvent
+local function ReloadScript()
+    LocalPlayer.PlayerGui.MenuGui.Menu.TabContents.Settings.Contents.LineFrame.ColorPicking.Enabled = false
+    LocalPlayer.PlayerGui.MenuGui.Menu.TabContents.Settings.Contents.LineFrame.ColorPicking.Enabled = true
+end
+
+Notify({
+    Title = "Multicolor Line",
+    Description = "Activated! (no toggle)",
+    Duration = 5
+})
+
+hookinstance(ScriptNotify, Activator)
+ReloadScript()
+task.delay(0.1, function()
+    Activator:FireServer()
+end)
+Toggle(true)
